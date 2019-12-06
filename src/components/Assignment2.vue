938b9920f58fbddcba945c948a3309379a43490f
Gan Hong Jun <hongjun87@gmail.com>
2019-12-06 15:44:38 +0800

    Dynamic binding of data

--- src/components/Assignment2.vue
+++ src/components/Assignment2.vue
@@ -1,12 +1,26 @@
 <template>
-  <p>2</p>
+  <div>
+    <v-row>
+      <v-col>
+        <v-card dark>
+          <v-card-title>Task 1</v-card-title>
+          <v-card-subtitle>Dynamic binding of data</v-card-subtitle>
+          <v-card-text>
+            <v-text-field v-model="name"></v-text-field>
+            <p>{{ name }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+  
 </template>
 
 <script>
   export default {
     name: 'Assignment2',
     data: () => ({
-
+      name: 'Thanos'
     })
   }
 </script>
