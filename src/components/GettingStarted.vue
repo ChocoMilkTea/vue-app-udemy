3f79abf3546346b73258b0e22351435287906b10
Gan Hong Jun <hongjun87@gmail.com>
2019-12-06 12:05:21 +0800

    Add new basic card styling for 'Getting Started'

--- src/components/GettingStarted.vue
+++ src/components/GettingStarted.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>    
-    <h1 v-once>{{ title }}</h1>
-    <p>
-      {{ sayHello() }} - <a v-bind:href="link">Google</a>
-    </p>    
-    <p v-html="finishedLink"></p>
+  <div>
+    <v-row>
+      <v-col>
+        <v-card dark>
+          <v-card-title>Task 1</v-card-title>
+          <v-card-subtitle>Print out text</v-card-subtitle>
+          <v-card-text>
+            <h1 v-once>{{ title }}</h1>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-card dark>
+          <v-card-title>Task 2</v-card-title>
+          <v-card-subtitle>Print out using function and binding of a link</v-card-subtitle>
+          <v-card-text>
+            <p>
+              {{ sayHello() }} - <a v-bind:href="link">Google</a>
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-card dark>
+          <v-card-title>Task 3</v-card-title>
+          <v-card-subtitle>Print out raw html</v-card-subtitle>
+          <v-card-text>
+            <p v-html="finishedLink"></p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>        
+    
   </div>  
 </template>
 
