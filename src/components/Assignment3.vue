967fd5c3b7be4929e0f07e05c9910b440691fada
Gan Hong Jun <hongjun87@gmail.com>
2019-12-06 17:18:03 +0800

    Add styling without using classes

--- src/components/Assignment3.vue
+++ src/components/Assignment3.vue
@@ -18,6 +18,23 @@
         </v-card>
       </v-col>
     </v-row>    
+    <v-row>
+      <v-col>
+        <v-card dark>
+          <v-card-title>Task 2</v-card-title>
+          <v-card-subtitle>Dynamic styling without using classes</v-card-subtitle>
+          <v-card-actions>
+            <div class="demo" :style="{'background-color': task2Color}"></div>
+            <div class="demo" :style="myStyle"></div>
+            <div class="demo" :style="[myStyle, {height: width + 'px'}]"></div>
+            <v-spacer></v-spacer>
+            <v-text-field v-model="task2Color"></v-text-field>
+            <v-spacer></v-spacer>
+            <v-text-field v-model="width"></v-text-field>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>    
   </div>
 </template>
 
@@ -48,13 +65,21 @@
     name: 'Assignment3',
     data: () => ({
       attachRed: false,
-      color: 'green'
+      color: 'green',
+      task2Color: 'gray',
+      width: 100
     }),
     computed: {
       divClasses() {
         return {
           red: this.attachRed,
           blue: !this.attachRed
+        }
+      },
+      myStyle() {
+        return {
+          backgroundColor: this.task2Color,
+          width: this.width + 'px'
         }
       }
     }
