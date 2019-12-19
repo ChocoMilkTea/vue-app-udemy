b9f73a39d9c66f16220678f5cdfe3aaf58aec17b
Gan Hong Jun <hongjun87@gmail.com>
2019-12-19 11:30:49 +0800

    Adding v-for loop with index

--- src/components/Assignment4.vue
+++ src/components/Assignment4.vue
@@ -1,32 +1,52 @@
 <template>
-  <v-row>
-    <v-col>
-      <v-card dark>
-        <v-card-title>Task 1</v-card-title>
-        <v-card-subtitle>Conditional Rendering with if-else</v-card-subtitle>
-        <v-card-text>
-          <p v-if="show">You can see me!<span>Hello!</span></p>
-          <p v-else-if="abc">Now you see me!</p>
-          <p v-else>Only now you see me!</p>          
-          <p v-show="show">Do you also see me?</p>
-          <template v-if="show">
-            <h1>Heading</h1>
-            <p>Inside a template</p>
-          </template>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn @click="show = !show">Switch</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col>
+        <v-card dark>
+          <v-card-title>Task 1</v-card-title>
+          <v-card-subtitle>Conditional Rendering with if-else</v-card-subtitle>
+          <v-card-text>
+            <p v-if="show">You can see me!<span>Hello!</span></p>
+            <p v-else-if="abc">Now you see me!</p>
+            <p v-else>Only now you see me!</p>          
+            <p v-show="show">Do you also see me?</p>
+            <template v-if="show">
+              <h1>Heading</h1>
+              <p>Inside a template</p>
+            </template>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click="show = !show">Switch</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card dark>
+          <v-card-title>Task 2</v-card-title>
+          <v-card-subtitle>Rendering list with v-for</v-card-subtitle>
+          <v-card-text>
+            <ul>
+              <li v-for="(ingredient, i) in ingredients" v-bind:key ="ingredient">{{ ingredient }} ({{ i }})</li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'Assignment4',
     data: () => ({
-      show: true
+      show: true,
+      ingredients: ['meat', 'fruit', 'cookies'],
+      persons: [
+        { name: 'Max', age: 27, color: 'red' },
+        { name: 'Anna', age: 'unknown', color: 'blue' }
+      ]
     })
   }
 </script>
